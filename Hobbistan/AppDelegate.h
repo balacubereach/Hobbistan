062439ac809eb50f53be954d6b1eb5f043337db0
kavitha <kavitha@uitouxsolutions.com>
2016-07-20 17:13:40 +0530

    Initial Commit

---
+++ Hobbistan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hobbistan
+//
+//  Created by UITOUX on 20/07/16.
+//  Copyright © 2016 UITOUX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hobbistan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hobbistan
+//
+//  Created by UITOUX on 20/07/16.
+//  Copyright © 2016 UITOUX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
